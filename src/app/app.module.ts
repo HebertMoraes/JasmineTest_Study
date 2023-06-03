22d9b11e6fc1de4047030aeff2ebd02a1a2cc6fd
Hebert Moraes <70861445+HebertMoraes@users.noreply.github.com>
2023-06-03 14:19:16 -0300

    Estrutura base iniciada
    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageModule } from './components/home-page/home-page.module';
+import { HeaderModule } from './components/header/header.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    HomePageModule, 
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
